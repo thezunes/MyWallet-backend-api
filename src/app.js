0dcff78efbb22bbe41b2e76cc6df8455ec1ad634
Lucas Nunes <120993347+thezunes@users.noreply.github.com>
2023-04-24 19:32:02 -0300

    chore: adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -1,2 +1,61 @@
-import { Express } from "express";
+import express from "express";
 import cors from "cors"
+import {MongoClient} from "mongodb"
+import dotenv from "dotenv"
+import joi from "joi";
+import { v4 as uuid } from 'uuid';
+import bcrypt from 'bcrypt';
+
+
+
+
+const app = express()
+const token = uuid()
+
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+
+let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
+
+const userSchema = joi.object({
+name: joi.string().required(),
+email: joi.string().email().required(),
+password: joi.string().min(3).required()
+
+});
+
+app.post("/signup", async(req, res) => {
+const {name, email, password} = req.body
+const user = {name,email,password}
+const encryptedPassword = bcrypt.hashSync(password, 10);
+const validation = userSchema.validate(user, { abortEarly: false })
+ if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  
+try {
+const newUser = {name: name,email: email,password: encryptedPassword}
+const isRegistered = db.collection("users").findOne({email: email})
+
+if(isRegistered){ return res.status(409).send("Email já cadastrado")} 
+res.sendStatus(201)
+db.collection("users").insertOne(newUser)
+}
+catch(err){
+res.status(401).send(err)
+
+}
+
+})
+
+
+
+
+ app.listen(process.env.PORT, () => console.log(`Server is Running at port ${process.env.PORT}`))
